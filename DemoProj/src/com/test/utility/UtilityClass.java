9db162e6e14e57dfb324a57fc51d6c7ca9215eab
Dev2 <Dev2@gmail.com>
2021-02-06 12:33:14 +0530

    sdfsdf

--- DemoProj/src/com/test/utility/UtilityClass.java
+++ DemoProj/src/com/test/utility/UtilityClass.java
@@ -8,10 +8,7 @@ public Date getDate() {
 }
 
 public void m1() {
-<<<<<<< HEAD
-	System.out.println("utility m1 method ....abcd");
-=======
 	System.out.println("utility m1 method :123");
->>>>>>> branch 'master' of https://github.com/kishor1234358/javaproject.git
+	System.out.println("abc123");
 }
 }
